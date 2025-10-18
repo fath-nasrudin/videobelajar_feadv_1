8805575dc8e958bb397208696ac6e98706cf3704
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-10-18 13:18:39 +0700

    Add link in the buttons

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,6 +1,8 @@
 import { Header } from "@/components/header";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import Link from "next/link";
 
 const fieldIds = {
   email: "reg-email",
@@ -87,40 +89,41 @@ export default function RegisterPage() {
                 </span>
               </div>
             </div>
+            <div className="flex flex-col gap-2">
+              <Link href="/login">
+                <Button type="submit" variant={"primary"} className="w-full">
+                  Daftar
+                </Button>
+              </Link>
 
-            <button type="submit" className="w-full btn btn--primary">
-              Daftar
-            </button>
+              <Link href="/login">
+                <Button
+                  variant={"primaryShadowed"}
+                  type="button"
+                  className="w-full"
+                >
+                  Masuk
+                </Button>
+              </Link>
 
-            <a href="./login.tw.html">
-              <button
-                type="button"
-                className="w-full btn btn--primary-shadowed"
-              >
-                Masuk
-              </button>
-            </a>
-
-            <div className="relative my-4">
-              <div className="absolute inset-0 flex items-center">
-                <div className="w-full border-t border-gray-300"></div>
+              <div className="relative my-4">
+                <div className="absolute inset-0 flex items-center">
+                  <div className="w-full border-t border-gray-300"></div>
+                </div>
+                <div className="relative flex justify-center text-xs">
+                  <span className="px-2 bg-white text-gray-500">atau</span>
+                </div>
               </div>
-              <div className="relative flex justify-center text-xs">
-                <span className="px-2 bg-white text-gray-500">atau</span>
-              </div>
+
+              <Button type="button" variant={"outline"}>
+                <img
+                  src="https://www.svgrepo.com/show/475656/google-color.svg"
+                  alt="Google"
+                  className="w-5 h-5 mr-2"
+                />
+                Daftar dengan Google
+              </Button>
             </div>
-
-            <button
-              type="button"
-              className="w-full flex items-center justify-center border border-gray-300 py-2 rounded-md text-sm font-medium hover:bg-gray-50 transition"
-            >
-              <img
-                src="https://www.svgrepo.com/show/475656/google-color.svg"
-                alt="Google"
-                className="w-5 h-5 mr-2"
-              />
-              Daftar dengan Google
-            </button>
           </form>
         </div>
       </main>
