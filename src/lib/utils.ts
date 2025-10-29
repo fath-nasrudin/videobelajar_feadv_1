1a63e2d0845ba127797f376d4f0aa60b431f9c8d
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-10-29 15:46:37 +0700

    Add formatPrice utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatPrice(number: number, withCurrency = false) {
+  if (withCurrency) {
+    return number.toLocaleString("id-ID", {
+      style: "currency",
+      currency: "IDR",
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    });
+  }
+
+  return number.toLocaleString("id-ID");
 }
