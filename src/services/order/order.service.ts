778319eb873e989886d9fb30b0776a89bcfc3e45
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-11-13 15:05:24 +0700

    Attach course object to order in order service

--- src/services/order/order.service.ts
+++ src/services/order/order.service.ts
@@ -1,3 +1,4 @@
+import { getCourseDetail } from "@/data/courses";
 import { FetchError } from "@/lib/errors/FetchError";
 import { randId } from "@/lib/localstorage.helper";
 import { CreateOrderInput, Order, UpdateOrderInput } from "@/types";
@@ -10,7 +11,12 @@ export async function getOrderlist(): Promise<Order[]> {
     if (!response.ok) {
       throw new FetchError("Failed to fetch orders", response.status);
     }
-    return response.json();
+    const orders: Order[] = await response.json();
+    orders.forEach((order) => {
+      order.course = getCourseDetail(order.courseId);
+      return order;
+    });
+    return orders;
   } catch (err) {
     if (err instanceof FetchError) throw err;
     if (err instanceof Error) throw new FetchError(err.message, 500);
@@ -44,7 +50,8 @@ export async function createOrder(orderData: CreateOrderInput): Promise<Order> {
       );
     }
 
-    const createdOrder = await response.json();
+    const createdOrder: Order = await response.json();
+    createdOrder.course = getCourseDetail(createdOrder.courseId);
     return createdOrder;
   } catch (err) {
     if (err instanceof FetchError) throw err;
